e98f85e2067b2e9675f5880bbcd0447ea8ae484c
Jacob Barber <jacoby6000@gmail.com>
2023-09-29 21:44:22 -0500

    embed per server

--- src/server_browser_discord_bot/main.py
+++ src/server_browser_discord_bot/main.py
@@ -1,4 +1,5 @@
 from ast import Dict
+import colorsys
 import traceback
 from typing import Any, Optional
 import discord
@@ -47,64 +48,92 @@ def get_server_info():
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user.name} ({client.user.id})")
+
+    await reset_channel()
+
+    message_buffer: Dict[str, discord.Embed] = {}
     while not client.is_closed():
         try:
             print("Sleeping for " + str(UPDATE_INTERVAL) + " seconds")
             await asyncio.sleep(UPDATE_INTERVAL)  # Wait a minute before updating again
 
             channel = client.get_channel(CHANNEL_ID)
-            
+
             server_info = get_server_info()
             if not server_info:
                 continue
 
-            # Get all messages in the channel
-            messages = await channel.history(limit=None).flatten()
-
             print(f"Found {len(server_info['servers'])} servers")
 
-            servers = [[
-                server['name'],
-                server['current_map'],
-                server['player_count'],
-                ":reversecheckmark:" if server.get('password_protected', False) else ":x:",
-                server['description']
-            ] for server in server_info['servers']]
+            for server in server_info['servers']:
+                unique_id = server['unique_id']
+                name = server['name']
+                current_map = server['current_map']
+                player_count = server['player_count']
+                max_players = server['max_players']
+                password_protected = ":reversecheckmark:" if server.get('password_protected', False) else ":x:"
+                description = server['description']
 
-            new_embed = discord.Embed(
-                title="Chivalry 2 Community Server List", 
-                color=0x00ccff,
-            )
+                embed = discord.Embed(title=name, color=hash_to_color(hash(name)))
+                embed.add_field(name="Map", value=current_map, inline=True)
+                embed.add_field(name="Players", value=str(player_count) + " / " + str(max_players), inline=True)
+                embed.add_field(name="Password Protected", value=password_protected, inline=True)
+                embed.add_field(name="Description", value=description, inline=True)
 
-            new_embed.set_footer(text="Install the launcher to join private servers: https://github.com/Chiv2-Community/UnchainedLauncher/releases/latest")
+                # Check if we've already posted about this server
+                if unique_id in message_buffer:
+                    # Update the message
+                    await message_buffer[unique_id].edit(embed=embed)
+                else:
+                    # Otherwise, create a new message
+                    sent_message = await channel.send(embed=embed, silent=True)
+                    message_buffer[unique_id] = sent_message
 
-            servers.sort(key=lambda x: x[0])
+            # Check for servers that no longer exist and delete their messages
+            to_delete = []
+            current_server_ids = {server['unique_id'] for server in server_info['servers']}
+            for message_id, _ in message_buffer.items():
+                if message_id not in current_server_ids:
+                    to_delete.append(message_id)
 
-            for [name, current_map, player_count, password_protected, description] in servers:
-                current_server_message = filter(lambda x: x.embeds and x.embeds[0].title == name, messages)
-                new_embed.add_field(
-                    name=name, 
-                    inline=True,
-                    value=f"**Map:** {current_map}\n**Players:** {player_count}\n**Password Protected:** {password_protected}\n**Description:** {description}"
-                )
+            for message_id in to_delete:
+                await message_buffer[message_id].delete()
+                del message_buffer[message_id]
 
-            
-            # Fetch last message in the channel
-            last_message = None
-            async for message in channel.history(limit=1):
-                last_message = message
-
-
-            # If the last message was sent by our bot, we'll edit it. If not, we'll send a new message.
-            if last_message and last_message.author == client.user:
-                print("Editing last message")
-                await last_message.edit(content=None, embed=new_embed)
-            else:
-                print("Sending new message")
-                await channel.send(new_embed)
         except Exception as e:
             print(f"Error: {e}")
             traceback.print_exc()
+
+async def reset_channel():
+    try:
+        print("Resetting channel")
+        channel = client.get_channel(CHANNEL_ID)
+        messages = channel.history(limit=None)
+        async for message in messages:
+            print(f"Deleting message {message.id}")
+            await message.delete()
+    except Exception as e:
+        print(f"Error: {e}")
+        traceback.print_exc()
+
+def hash_to_color(hash_int: int):
+    # Convert the hash to an integer
+    
+    # Use the integer to get values between 0 and 1 for HSV components
+    h = (hash_int & 0xFF) / 255.0
+    s = ((hash_int >> 8) & 0xFF) / 255.0 * 0.5 + 0.5   # Ensuring saturation is vibrant
+    v = ((hash_int >> 16) & 0xFF) / 255.0 * 0.5 + 0.5  # Ensuring value/brightness is vibrant
+    
+    # Convert HSV to RGB
+    r, g, b = colorsys.hsv_to_rgb(h, s, v)
+    
+    # Convert RGB from 0-1 to 0-255
+    r = int(r * 255)
+    g = int(g * 255)
+    b = int(b * 255)
+    
+    # Return in 0xRRGGBB format
+    return (r << 16) | (g << 8) | b
 
 if __name__ == "__main__":
     print("Starting bot")
